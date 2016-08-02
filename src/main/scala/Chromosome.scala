cf80509a72e5b653966749af7635e8088ab31326
Vikram Ramesh <vikram132@gmail.com>
2016-08-01 20:00:13 -0700

    Added implicit parameter to represent ideal fitness for input set

--- src/main/scala/Chromosome.scala
+++ src/main/scala/Chromosome.scala
@@ -1,4 +1,5 @@
 import scala.util.Random
+import Chromosome._
 
 /**
   * Representation of a chromosome
@@ -9,25 +10,39 @@ object Chromosome {
 
   /**
     * create a chromozone with random values for the given size
-    * @param size
+    *
+    * @param size of the chromosome
     * @return
     */
-  def apply(size: Int): Chromosome = {
+  def apply(size: Int)(implicit IDEAL_FITNESS: IdealFitness): Chromosome = {
     val rawData = List.fill(size)(Random.nextDouble())
     val sum = rawData.sum
     new Chromosome(
-      data = rawData.map(_ / sum) //normalize
+      data = rawData.map(d ⇒ (d / sum) * 100) //normalize
     )
   }
 
   def mate(first: Chromosome, second: Chromosome): Chromosome = ???
+
+  //todo this is a bit overkill for now, but it might be useful to keep the aggregated input list for a better fitness function
+  /**
+    * Representation object for the ideal fitness calculated from the input set
+    *
+    * @param idealSum
+    */
+  case class IdealFitness(idealSum: Double)
+  object IdealFitness {
+    def apply(input: List[List[Int]]): IdealFitness = {
+      val combinedInputs = input.transpose.map(_.sum)
+      IdealFitness(combinedInputs.size * 1) //todo eventually add negatives
+    }
+  }
 }
 
-class Chromosome(data: List[Double]) {
+//todo implicitly add IdealFitness
+class Chromosome(data: List[Double])(implicit val IDEAL_FITNESS: IdealFitness) {
 
-  val IDEAL_SUM = data.size * 1 //todo eventually add negatives
-
-  lazy val fitness: Double = math.abs(data.sum - IDEAL_SUM)
+  lazy val fitness: Double = math.abs(data.sum - IDEAL_FITNESS.idealSum)
 
   /**
     * Calculate this chromosome's fitness
--- src/main/scala/InterestPrioritization.scala
+++ src/main/scala/InterestPrioritization.scala
@@ -2,7 +2,15 @@
   * Created by vikram on 8/1/16.
   */
 object InterestPrioritization extends App {
+  import Chromosome._
 
   println("hello, cool App can be used as a cleaner entry point!")
+
+  val input = List(List(1, 2, 3), List(4, 5, 3), List(6, 7, 8))
+
+  implicit val IDEAL_FITNESS = IdealFitness(input)
+
+  val c = Chromosome(3)
+
 
 }
